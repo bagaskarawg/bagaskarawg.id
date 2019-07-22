189680c5be333190dbb4bfe3e99f95e93659b798
Bagaskara Wisnu Gunawan <bagaswisnu07@gmail.com>
2019-07-22 19:48:24 +0700

    feat: initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->fill([
+            'name' => 'Bagaskara Wisnu Gunawan',
+            'email' => 'me@bagaskarawg.id',
+            'password' => bcrypt('bagaskarawg'),
+            'email_verified_at' => now(),
+        ]);
+        $user->save();
+    }
+}

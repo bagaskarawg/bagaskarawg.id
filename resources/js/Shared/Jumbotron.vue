7a75a577d633bb374c2823e96584c5d5fd3dedc5
Bagaskara Wisnu Gunawan <bagaswisnu07@gmail.com>
2019-10-27 10:09:38 +0700

    feat: remove style from jumbotron file

--- resources/js/Shared/Jumbotron.vue
+++ resources/js/Shared/Jumbotron.vue
@@ -5,17 +5,11 @@
         <img src="https://secure.gravatar.com/avatar/3bfa4c41912fe733d8244f0d2146b18a?s=200" class="rounded-full mx-auto" />
         Hello, I'm <h2 class="font-semibold text-2xl mb-2 text-white">Bagaskara Wisnu Gunawan</h2>
         a full-stack web developer with more than 4 years of experience developing web applications using <strong>Laravel</strong>, <strong>Ruby on Rails 3</strong>, and <strong>Vue.js</strong>.
-        <inertia-link href="#" class="block max-w-full w-64 mx-auto mt-4 p-4 border border-red text-white hover:bg-white hover:text-red-700 hover:font-semibold">More about me</inertia-link>
+        <inertia-link href="https://bagaskarawg.id/blog/who-am-i" id="about-me" class="block max-w-full w-64 mx-auto mt-4 p-4 border border-red text-white hover:bg-white hover:text-red-700 hover:font-semibold">More about me</inertia-link>
       </div>
     </div>
   </div>
 </template>
-
-<style>
-a {
-  transition: .25s linear;
-}
-</style>
 
 <script>
 export default {
